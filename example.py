b53b9ecc397ea16e70988fcfd5d86cc8c0bdfc2f
Kuldeep Verma <43984256+kuldeepv89@users.noreply.github.com>
2021-03-08 15:40:45 +0100

    Minor update to the example

--- example.py
+++ example.py
@@ -2,14 +2,13 @@ import numpy as np
 import ratios
 
 
-
-fname = '/Users/kuldeep/Dropbox/seismology/obs_data/kic012069424/freq/freq.dat'
+# Load the data
 names = ['l', 'n', 'freq', 'err']
 fmts = [int, int, float, float]
-obsFreq = np.genfromtxt(fname, dtype={'names': names, 'formats': fmts})
+obsFreq = np.genfromtxt('16CygA.txt', dtype={'names': names, 'formats': fmts})
 
-r02, r01, r10, r010, r012, r102 = ratios.ratios(obsFreq)
-print(r02)
+# Compute ratios
+r02, r01, r10 = ratios.ratios(obsFreq)
 
-#obsr012, covr012, icovr012 = ratios.ratio_and_cov(obsFreq, rtype='R02', nrealizations=10000)
-#print (obsr012)
+# Compute ratios as well as corresponding covariance matrix
+obsr02, covr02 = ratios.ratio_and_cov(obsFreq, rtype='R02', nrealizations=10000)
